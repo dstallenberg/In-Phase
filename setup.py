4e0fb88991b4835df913a2a50aaba1d8117cdb25
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2020-01-08 20:07:50 +0100

    Update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='QPE',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Group7',
+    author_email='',
+    description='Quantum phase estimation',
+    python_requires='>=3.6',
+)
