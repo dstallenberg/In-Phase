e884ef23e8d41f382873f42cdc3f8f02f9472b6e
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2020-01-13 14:29:16 +0100

    Update

--- src/quantum_phase_estimation/generator.py
+++ src/quantum_phase_estimation/generator.py
@@ -5,6 +5,9 @@ from src.quantum_phase_estimation.optimizer import optimize
 def generate_quantum_inspire_code(nancillas, qubits, unitary_operation, custom_prepare='# No custom preparation given by user'):
     # Check if QASM en then replace q[i] with q[i + nancilla] etc
 
+    if isinstance(unitary_operation, str) and 'QASM' in unitary_operation:
+        unitary_operation += "\n"
+
     for i in range(nancillas+2*qubits, -1, -1):
         if isinstance(unitary_operation, str) and 'QASM' in unitary_operation:
             unitary_operation += "\n"
