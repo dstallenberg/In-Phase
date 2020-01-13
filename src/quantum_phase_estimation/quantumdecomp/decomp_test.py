ac951cb0b257d6e1c759dc37e1869be926e2475a
Roffid <43887079+Roffid@users.noreply.github.com>
2020-01-13 09:16:42 +0100

    For dimitri2

--- src/quantum_phase_estimation/quantumdecomp/decomp_test.py
+++ src/quantum_phase_estimation/quantumdecomp/decomp_test.py
@@ -1,10 +1,11 @@
 import numpy as np
 from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import matrix_to_qsharp
-
+from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import U_to_CU
 w = np.exp((2j / 3) * np.pi)
 A = np.array([[1, 1, 1, 0],
                   [1, w, w * w, 0],
                   [1, w * w, w, 0],
                   [0, 0, 0, -1j*np.sqrt(3)]]) / np.sqrt(3)
 
-print(matrix_to_qsharp(A, 1, -1))
+
+print(U_to_CU(2,1,2, "Test1\nTest2\nTest3"))
