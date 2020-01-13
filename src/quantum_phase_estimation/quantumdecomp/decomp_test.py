344488c607886811b5d7dfde7aa665cc35445c9c
Roffid <43887079+Roffid@users.noreply.github.com>
2020-01-13 11:58:18 +0100

    Rotation phi calc

--- src/quantum_phase_estimation/quantumdecomp/decomp_test.py
+++ src/quantum_phase_estimation/quantumdecomp/decomp_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
-from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import matrix_to_qsharp
-from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import U_to_CU
+"""from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import matrix_to_qsharp
+from src.quantum_phase_estimation.quantumdecomp.quantum_decomp import U_to_CU"""
 w = np.exp((2j / 3) * np.pi)
 
 A = np.array([[1,1,1,0],
@@ -9,6 +9,6 @@ A = np.array([[1,1,1,0],
      [1,w*w,w,0],
      [0,0,0,-1j*np.sqrt(3)]]) / np.sqrt(3)
 
-print(matrix_to_qsharp(A,0,1))
-"""matrix = np.array([[0.758, 0], [1, 1]])
-print(math.acos(matrix[0,0]))"""
+
+matrix = np.array([[-1+5j, 0], [1, 1]])
+print(2*math.acos(matrix[0,0].real))
