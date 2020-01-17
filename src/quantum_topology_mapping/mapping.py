1fdebb32904bec55a8e21caac2695243b7f0ec34
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2020-01-17 10:01:31 +0100

    Update

--- src/quantum_topology_mapping/mapping.py
+++ src/quantum_topology_mapping/mapping.py
@@ -4,8 +4,7 @@ from src.quantum_topology_mapping.dijkstra import Graph
 def map_to_topology(graph):
     print(graph.dijkstra("a", "e"))
 
-
- graph = Graph([
+graph = Graph([
         ("a", "b", 7), ("a", "c", 9), ("a", "f", 14), ("b", "c", 10),
         ("b", "d", 15), ("c", "d", 11), ("c", "f", 2), ("d", "e", 6),
         ("e", "f", 9)])
