6eae92cd7bd5db572c0ea8f0db7982d5c463ab15
Mio Poortvliet <miodragpoortvliet@gmail.com>
2020-01-22 12:28:01 +0100

    Cleaned up code some

--- src/tests/mapping/plotting.py
+++ src/tests/mapping/plotting.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from src.quantum_phase_estimation.error_estimation import error_estimate
 
 def heatmap(data, show=True):
     fig = plt.figure(figsize=(7,7))
     ax = plt.gca()
 
+    # Show the matrix
     cs = ax.imshow(np.rot90(data), aspect='auto')
     fig.colorbar(cs)
 
+    # Set up xlabels
     ax.set_xlabel(r"Input phase $\varphi/2\pi$")
     xticks = ax.get_xticks()
     print(xticks)
     x_labels = ['{0:0.3f}'.format(x) for x in np.linspace(xticks[0]/xticks[-1], xticks[-1]/data.shape[0], len(xticks))]
     ax.set_xticklabels(x_labels)
 
-
-
+    # Set up ylabels
     ax.set_ylabel(r"Output phase $\varphi/2\pi$")
     yticks = ax.get_yticks()
     print(yticks)
@@ -29,19 +29,25 @@ def heatmap(data, show=True):
     if show:
         plt.show()
 
+
 def graph(data, show=True):
     fig = plt.figure(figsize=(7, 7))
     ax = plt.gca()
 
+    # Plot the points from QI
     ax.errorbar(np.linspace(0, 1, data.size),
                 data,
                 yerr=(np.repeat(0, data.shape[0]),
                       np.repeat(2**(-np.log2(data.shape[0])), data.shape[0])),
                 fmt='.',
+                ms=2,
+                capthick=0.5,
                 label="From QI")
 
+    # Plot the expected line
     ax.plot(np.linspace(0, 1, data.size), np.linspace(0, 1, data.size), label="Expected")
 
+    # Set up labels and legend
     ax.set_xlabel("Input phase [$\cdot 2\pi$]")
     ax.set_ylabel("Output phase [$\cdot 2\pi$]")
     plt.legend()
--- src/tests/mapping/process_data.py
+++ src/tests/mapping/process_data.py
@@ -4,19 +4,25 @@ from src.quantum_phase_estimation.classical_postprocessing import remove_degener
 
 def to_array(result, bit, nancilla):
     data = np.zeros(shape=(2, 2**bit, 2**nancilla))
+
     for i, r in enumerate(result):
         values, keys = remove_degeneracy(r['histogram'], nancilla)
         keys = np.array([int(x, 2) for x in keys])
+
         for j in range(2**nancilla):
             if np.sum(keys==j):
                 data[0, i, j] = keys[keys==j]
                 data[1, i, j] = values[keys==j]
+
     return data
+
 
 def sort_array(data):
     ind = np.unravel_index(np.argsort(data[0,::,::], axis=1), shape=data[0].shape)
+
     data[0] = data[0][ind]
     data[1] = data[1][ind]
+
     return data
 
 
@@ -32,8 +38,10 @@ def find_keys(data):
 
 def decimal_to_binary_fracion(list, bit):
     output = np.zeros(shape=list.size, dtype=np.float)
+
     for i, dec in enumerate(list):
         for j, b in enumerate( str(bin(dec))[2:].rjust(bit, '0') ):
             output[i] += 2**(-j-1)*int(b)
+
     return output
 
